5060b319587c6934fc5d50f64e5e1e4f381dec9c
Dustin Frisch <fooker@lab.sh>
2020-11-10 11:02:02 +0100

    fixup! NMS-12967: Fix exploding flow into buckets

--- main/src/test/java/org/opennms/nephron/FlowAnalyzerTest.java
+++ main/src/test/java/org/opennms/nephron/FlowAnalyzerTest.java
@@ -45,8 +45,15 @@ import java.util.function.LongFunction;
 import org.apache.beam.sdk.testing.PAssert;
 import org.apache.beam.sdk.testing.TestPipeline;
 import org.apache.beam.sdk.testing.TestStream;
+import org.apache.beam.sdk.transforms.Combine;
 import org.apache.beam.sdk.transforms.Create;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.MapElements;
+import org.apache.beam.sdk.transforms.PTransform;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.transforms.SimpleFunction;
 import org.apache.beam.sdk.transforms.View;
+import org.apache.beam.sdk.transforms.WithKeys;
 import org.apache.beam.sdk.transforms.windowing.IntervalWindow;
 import org.apache.beam.sdk.values.KV;
 import org.apache.beam.sdk.values.PCollection;
@@ -411,7 +418,7 @@ public class FlowAnalyzerTest {
     }
 
     @Test
-    public void testAttachedTimestamps() {
+    public void testAttachedTimestamps() throws Exception {
         final org.joda.time.Instant start = org.joda.time.Instant.EPOCH;
         final Duration WS = Duration.standardSeconds(10);
 
@@ -423,61 +430,79 @@ public class FlowAnalyzerTest {
 
         // Does not align with window
         final FlowDocument flow1 = new SyntheticFlowBuilder()
-                .withExporter("SomeFs", "SomeFid", 99)
+                .withExporter("TestFlows", "Flow1", 99)
                 .withSnmpInterfaceId(98)
                 .withApplication("SomeApplication")
                 .withHostnames("first.src.example.com", "second.dst.example.com")
                 .withFlow(Instant.ofEpochSecond(17), Instant.ofEpochSecond(32),
                           "10.0.0.1", 88,
                           "10.0.0.2", 99,
-                          42)
+                          (32 - 17) * 100)
                 .build()
                 .get(0);
+        final Groupings.ExporterInterfaceKey key1 = Groupings.ExporterInterfaceKey.from(flow1);
 
         // Start aligns with window
         final FlowDocument flow2 = new SyntheticFlowBuilder()
-                .withExporter("SomeFs", "SomeFid", 99)
+                .withExporter("TestFlows", "Flow2", 99)
                 .withSnmpInterfaceId(98)
                 .withApplication("SomeApplication")
                 .withHostnames("first.src.example.com", "second.dst.example.com")
                 .withFlow(Instant.ofEpochSecond(10), Instant.ofEpochSecond(32),
                           "10.0.0.1", 88,
                           "10.0.0.2", 99,
-                          42)
+                          (32 - 10) * 200)
                 .build()
                 .get(0);
+        final Groupings.ExporterInterfaceKey key2 = Groupings.ExporterInterfaceKey.from(flow2);
 
         // Start and end aligns with window
         final FlowDocument flow3 = new SyntheticFlowBuilder()
-                .withExporter("SomeFs", "SomeFid", 99)
+                .withExporter("TestFlows", "Flow3", 99)
                 .withSnmpInterfaceId(98)
                 .withApplication("SomeApplication")
                 .withHostnames("first.src.example.com", "second.dst.example.com")
                 .withFlow(Instant.ofEpochSecond(10), Instant.ofEpochSecond(40),
                           "10.0.0.1", 88,
                           "10.0.0.2", 99,
-                          42)
+                          (40 - 10) * 300)
                 .build()
                 .get(0);
+        final Groupings.ExporterInterfaceKey key3 = Groupings.ExporterInterfaceKey.from(flow3);
 
         // Does not align with window but spans one more bucket with wrong alignment
         final FlowDocument flow4 = new SyntheticFlowBuilder()
-                .withExporter("SomeFs", "SomeFid", 99)
+                .withExporter("TestFlows", "Flow4", 99)
                 .withSnmpInterfaceId(98)
                 .withApplication("SomeApplication")
                 .withHostnames("first.src.example.com", "second.dst.example.com")
                 .withFlow(Instant.ofEpochSecond(12), Instant.ofEpochSecond(37),
                           "10.0.0.1", 88,
                           "10.0.0.2", 99,
-                          42)
+                          (37 - 12) * 400)
                 .build()
                 .get(0);
+        final Groupings.ExporterInterfaceKey key4 = Groupings.ExporterInterfaceKey.from(flow4);
+
+        final FlowDocument flow5 = new SyntheticFlowBuilder()
+                .withExporter("TestFlows", "Flow5", 99)
+                .withSnmpInterfaceId(98)
+                .withApplication("SomeApplication")
+                .withHostnames("first.src.example.com", "second.dst.example.com")
+                .withFlow(Instant.ofEpochSecond(23), Instant.ofEpochSecond(27),
+                          "10.0.0.1", 88,
+                          "10.0.0.2", 99,
+                          (27 - 23) * 500)
+                .build()
+                .get(0);
+        final Groupings.ExporterInterfaceKey key5 = Groupings.ExporterInterfaceKey.from(flow5);
 
         final TestStream<FlowDocument> flows = TestStream.create(new FlowDocumentProtobufCoder())
                                                          .addElements(TimestampedValue.of(flow1, org.joda.time.Instant.EPOCH.plus(WS.multipliedBy(5))),
                                                                       TimestampedValue.of(flow2, org.joda.time.Instant.EPOCH.plus(WS.multipliedBy(5))),
                                                                       TimestampedValue.of(flow3, org.joda.time.Instant.EPOCH.plus(WS.multipliedBy(5))),
-                                                                      TimestampedValue.of(flow4, org.joda.time.Instant.EPOCH.plus(WS.multipliedBy(5))))
+                                                                      TimestampedValue.of(flow4, org.joda.time.Instant.EPOCH.plus(WS.multipliedBy(5))),
+                                                                      TimestampedValue.of(flow5, org.joda.time.Instant.EPOCH.plus(WS.multipliedBy(5))))
                                                          .advanceWatermarkToInfinity();
 
         final PCollection<FlowDocument> output = p.apply(flows)
@@ -485,9 +510,30 @@ public class FlowAnalyzerTest {
 
         PAssert.that("Bucket 0", output).inWindow(window.apply(0)).containsInAnyOrder();
         PAssert.that("Bucket 1", output).inWindow(window.apply(1)).containsInAnyOrder(flow1, flow2, flow3, flow4);
-        PAssert.that("Bucket 2", output).inWindow(window.apply(2)).containsInAnyOrder(flow1, flow2, flow3, flow4);
+        PAssert.that("Bucket 2", output).inWindow(window.apply(2)).containsInAnyOrder(flow1, flow2, flow3, flow4, flow5);
         PAssert.that("Bucket 3", output).inWindow(window.apply(3)).containsInAnyOrder(flow1, flow2, flow3, flow4);
         PAssert.that("Bucket 4", output).inWindow(window.apply(4)).containsInAnyOrder();
+
+        final PCollection<KV<Groupings.CompoundKey, Aggregate>> aggregates = output.apply(ParDo.of(new Groupings.KeyByExporterInterface()));
+
+        PAssert.that("Bytes 0", aggregates).inWindow(window.apply(0)).containsInAnyOrder();
+        PAssert.that("Bytes 1", aggregates).inWindow(window.apply(1)).containsInAnyOrder(
+                KV.of(key1, new Aggregate(300, 0, null)), // 100/s * 3s
+                KV.of(key2, new Aggregate(2000, 0, null)), // 200/s * 10s
+                KV.of(key3, new Aggregate(3000, 0, null)), // 300/s * 10s
+                KV.of(key4, new Aggregate(3200, 0, null))); // 400/s * 8s
+        PAssert.that("Bytes 2", aggregates).inWindow(window.apply(2)).containsInAnyOrder(
+                KV.of(key1, new Aggregate(1000, 0, null)), // 100/s * 10s
+                KV.of(key2, new Aggregate(2000, 0, null)), // 200/s * 10s
+                KV.of(key3, new Aggregate(3000, 0, null)), // 300/s * 10s
+                KV.of(key4, new Aggregate(4000, 0, null)), // 400/s * 10s
+                KV.of(key5, new Aggregate(2000, 0, null))); // 500/s * 4s
+        PAssert.that("Bytes 3", aggregates).inWindow(window.apply(3)).containsInAnyOrder(
+                KV.of(key1, new Aggregate(200, 0, null)), // 100/s * 2s
+                KV.of(key2, new Aggregate(400, 0, null)), // 200/s * 2s
+                KV.of(key3, new Aggregate(3000, 0, null)), // 300/s * 10s
+                KV.of(key4, new Aggregate(2800, 0, null))); // 400/s * 7s
+        PAssert.that("Bytes 4", aggregates).inWindow(window.apply(4)).containsInAnyOrder();
 
         p.run();
     }
